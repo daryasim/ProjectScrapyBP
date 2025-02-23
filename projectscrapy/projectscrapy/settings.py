268129644e747e198dcaaaf34c5f0d249f794f49
daryasim <dashasimakova8@gmail.com>
2025-02-23 13:38:43 +0100

    Scrapeops monitoring settings

--- projectscrapy/projectscrapy/settings.py
+++ projectscrapy/projectscrapy/settings.py
@@ -5,12 +5,20 @@ NEWSPIDER_MODULE = "projectscrapy.spiders"
 
 FEED_EXPORT_INDENT = 4
 
+FEEDS = {
+    '../../data/output_data.jl': {
+        'format': 'jl',
+        'encoding': 'utf8',
+        'store_empty': False,
+    },
+}
+
 DOWNLOAD_DELAY = 1
 
 ITEM_PIPELINES = {
    'projectscrapy.pipelines.CleanTextPipeline': 1,
    'projectscrapy.pipelines.PreprocessingPipeline': 2,
-   'projectscrapy.pipelines.SqlitePipeline': 300,
+   'projectscrapy.pipelines.SqlitePipeline': 3,
 }
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
@@ -20,4 +28,15 @@ COOKIES_ENABLED = False
 TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
 FEED_EXPORT_ENCODING = "utf-8"
 
+#scrapeops monitoring
+SCRAPEOPS_API_KEY = '5b43e41b-8e60-494f-a452-12c97db6de20'
+
+EXTENSIONS = {
+    'scrapeops_scrapy.extension.ScrapeOpsMonitor': 500,
+}
+
+DOWNLOADER_MIDDLEWARES = {
+    'scrapeops_scrapy.middleware.retry.RetryMiddleware': 550,
+    'scrapy.downloadermiddlewares.retry.RetryMiddleware': None,
+}
 
