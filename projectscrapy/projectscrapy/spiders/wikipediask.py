12954b4dc72e17f97814b14722013f817ecd4e13
daryasim <dashasimakova8@gmail.com>
2025-04-15 22:54:14 +0200

    minor

--- projectscrapy/projectscrapy/spiders/wikipediask.py
+++ projectscrapy/projectscrapy/spiders/wikipediask.py
@@ -10,20 +10,21 @@ class WikipediaskSpider(scrapy.Spider):
     allowed_domains = ['sk.wikipedia.org']
     start_urls = ['https://sk.wikipedia.org']
     url_limit = 1000
-    url_count = 0
+    url_count = set()
 
     def parse(self, response):
-        if self.url_count >= self.url_limit:
+        if len(self.url_count) >= self.url_limit:
             return
         links = response.css('a::attr(href)').getall()
         correct_linky = [link for link in links if
                          re.match(r'/wiki/[a-zA-Z]', link) and ':' not in link]
         for link in correct_linky:
-            if self.url_count >= self.url_limit:
-                break
-            self.url_count += 1
             next_page_url = response.urljoin(link)
-            yield response.follow(next_page_url, callback=self.parse_article)
+            if next_page_url not in self.url_count:
+                self.url_count.add(next_page_url)
+                if len(self.url_count) > self.url_limit:
+                    break
+                yield response.follow(next_page_url, callback=self.parse_article)
 
     def parse_article(self, response):
         soup = BeautifulSoup(response.body, 'html.parser')
@@ -38,13 +39,15 @@ class WikipediaskSpider(scrapy.Spider):
         )
         yield item
 
-        if self.url_count < self.url_limit:
-            linky_article = response.css('a::attr(href)').getall()
-            correct_linky_article = [link for link in linky_article if
-                                     re.match(r'/wiki/[a-zA-Z]', link) and ':' not in link]
-            for link in correct_linky_article:
-                if self.url_count >= self.url_limit:
+        if len(self.url_count) >= self.url_limit:
+            return
+        linky_article = response.css('a::attr(href)').getall()
+        correct_linky_article = [link for link in linky_article if
+                                 re.match(r'/wiki/[a-zA-Z]', link) and ':' not in link]
+        for link in correct_linky_article:
+            next_page_url = response.urljoin(link)
+            if next_page_url not in self.url_count:
+                self.url_count.add(next_page_url)
+                if len(self.url_count) > self.url_limit:
                     break
-                self.url_count += 1
-                next_page_url = response.urljoin(link)
                 yield response.follow(next_page_url, callback=self.parse_article)
